e8c30f575fef459cb3bda251022170bd0eb63a8c
AviTirto <avi.tirto@gmail.com>
2024-06-17 23:51:55 +0700

    API calls

--- StudyCardGenerator/study-card-generator/src/App.js
+++ StudyCardGenerator/study-card-generator/src/App.js
@@ -1,10 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import FlashcardList from './FlashcardList';
 import './app.css'
+import axios from 'axios';
 
 // For future self, incase forget: es7 react/redux --> auto boiler plate
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios
+          .get("https://opentdb.com/api.php?amount=10")
+          .then(res => {
+            setFlashcards(
+              res.data.results.map((questionItem, index) => {
+                const answer = questionItem.correct_answer
+                const options = [...questionItem.incorrect_answers, answer]
+                return {
+                  id: `${index}-${Date.now()}`,
+                  question: questionItem.question,
+                  answer: answer,
+                  options: options.sort(() => Math.random() - .5)
+                }
+              }) 
+            )
+          }
+          )
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData(); // Call the async function defined above
+  
+    // Note: No dependencies in the dependency array [], meaning this effect runs once after the initial render
+  }, []);
+
   return (
     <FlashcardList flashcards = {flashcards}/>
   );
